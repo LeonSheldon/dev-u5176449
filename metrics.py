88c627bdc26b9f8c7c3174ffb9eafb7e60061d22
Leon Sheldon <leonsheldon@gmail.com>
2015-05-14 12:14:04 +1000

    Added function to calculate AUROC to metrics.py

--- metrics.py
+++ metrics.py
@@ -1,6 +1,7 @@
 """The metrics module implements functions assessing prediction error for specific purposes."""
 
 import numpy as np
+import sklearn.metrics
 
 def trapz(x, y):
     """Trapezoidal rule for integrating
@@ -18,3 +19,10 @@ def trapz(x, y):
         area += 0.5*(sx[ix+1]-sx[ix])*(sy[ix+1]+sy[ix])
     return area
 
+def auc(y_true, y_score):
+    return sklearn.metrics.roc_auc_score(y_true, y_score)
+
+
+auc_test = ([0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.], [0,0,0,0,0,0,0,0,0,0,0])
+
+print(auc(auc_test[1], auc_test[0]))
